395f9e3992a0b32796ad8a58c68138d248608bfa
Will <williama149@gmail.com>
2024-08-15 10:20:48 +0100

    Created Game Page

---
+++ PacMERN-frontend/src/pages/GamePage/GamePage.jsx
@@ -0,0 +1,13 @@
+import { Container, Typography } from "@mui/material"
+import GlobalNavBar from "../../components/GlobalNavBar"
+
+export const GamePage = () => {
+    return (
+        <>
+        <GlobalNavBar/>
+        <Container>
+            <Typography>Hello World!</Typography>
+        </Container>
+        </>
+    )
+}
