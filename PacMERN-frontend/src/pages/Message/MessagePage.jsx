c486291c380cc7fbe6fd41cc0f3ae1a186b57809
abdallah <aharundd@gmail.com>
2024-08-22 15:15:38 +0100

    scroll through users in messages

--- PacMERN-frontend/src/pages/Message/MessagePage.jsx
+++ PacMERN-frontend/src/pages/Message/MessagePage.jsx
@@ -3,12 +3,19 @@ import { useNavigate } from "react-router-dom";
 import { getAllUsers } from "../../services/user";
 import { getToken } from "../../services/authentication";
 import { getUserWithMessagesAndConversations } from "../../services/message";
-import { Dialog, Box, Button, Container, Typography, TextField } from "@mui/material";
+import {
+    Dialog,
+    Box,
+    Button,
+    Container,
+    Typography,
+    TextField,
+} from "@mui/material";
 import ChatWindow from "./ChatWindow";
 import GlobalNavBar from "../../components/GlobalNavBar";
 import Footer from "../../components/footer";
 import FixedContainer from "../../components/Container";
-import theme from '../../assets/theme';
+import theme from "../../assets/theme";
 
 const MessagePage = () => {
     const [searchQuery, setSearchQuery] = useState("");
@@ -27,21 +34,25 @@ const MessagePage = () => {
             if (!token) {
                 throw new Error("No authentication token found");
             }
-            
+
             // Fetch current user data
             const userData = await getUserWithMessagesAndConversations();
             setCurrentUserId(userData.user._id);
 
             const users = await getAllUsers(token);
-            const otherUsers = users.filter(user => user._id !== userData.user._id);
+            const otherUsers = users.filter(
+                (user) => user._id !== userData.user._id
+            );
             setAllUsers(otherUsers);
             setFilteredUsers(otherUsers);
             setError(null);
         } catch (error) {
             console.error("Error fetching users:", error);
             setError("Failed to load users. " + error.message);
-            if (error.message === 'Authentication failed. Please log in again.') {
-                navigate('/login');
+            if (
+                error.message === "Authentication failed. Please log in again."
+            ) {
+                navigate("/login");
             }
         } finally {
             setIsLoading(false);
@@ -54,8 +65,10 @@ const MessagePage = () => {
 
     const filterUsers = useCallback(() => {
         const lowercaseQuery = searchQuery.toLowerCase();
-        return allUsers.filter((user) =>
-            user._id !== currentUserId && user.name.toLowerCase().includes(lowercaseQuery)
+        return allUsers.filter(
+            (user) =>
+                user._id !== currentUserId &&
+                user.name.toLowerCase().includes(lowercaseQuery)
         );
     }, [searchQuery, allUsers, currentUserId]);
 
@@ -75,36 +88,63 @@ const MessagePage = () => {
     if (error) return <div>{error}</div>;
 
     return (
-        <Container className='messages-container' sx={{ display: 'flex', flexDirection: 'column', minHeight: '10vh' }}>
+        <Container
+            className='messages-container'
+            sx={{ display: "flex", flexDirection: "column", minHeight: "10vh" }}
+        >
             <GlobalNavBar />
             <Typography
-                variant="h4"
-                sx={{ 
-                    fontFamily: theme.typography.retro, 
-                    textAlign: 'center', 
-                    mb: 3, 
-                    color: 'black',
+                variant='h4'
+                sx={{
+                    fontFamily: theme.typography.retro,
+                    textAlign: "center",
+                    mb: 3,
+                    color: "black",
                 }}
             >
                 Messages
             </Typography>
             <FixedContainer>
-                <Box sx={{ flex: 1, display: 'flex', justifyContent: 'center', alignItems: 'center', padding: '0rem', }}>
-                    <Box sx={{ width: '100%', maxWidth: '600px' , margin:'0rem',}}>
-                        <Typography variant="h5" sx={{ fontFamily: theme.typography.retro, fontSize: '1.5rem', fontWeight: 'bold', marginBottom: '0rem', minHeight: '5vh' }}>
+                <Box
+                    sx={{
+                        flex: 1,
+                        display: "flex",
+                        justifyContent: "center",
+                        alignItems: "center",
+                        padding: "0rem",
+                    }}
+                >
+                    <Box
+                        sx={{
+                            width: "100%",
+                            maxWidth: "600px",
+                            margin: "0rem",
+                        }}
+                    >
+                        <Typography
+                            variant='h5'
+                            sx={{
+                                fontFamily: theme.typography.retro,
+                                fontSize: "1.5rem",
+                                fontWeight: "bold",
+                                marginTop: "2rem",
+                                marginBottom: "1rem",
+                                minHeight: "5vh",
+                            }}
+                        >
                             Search Users
                         </Typography>
-                        <Box sx={{ marginBottom: '5rem' }}>
+                        <Box sx={{ marginBottom: "1rem" }}>
                             <TextField
                                 fullWidth
-                                variant="outlined"
+                                variant='outlined'
                                 value={searchQuery}
                                 onChange={(e) => setSearchQuery(e.target.value)}
                                 placeholder='...'
                                 sx={{
-                                    '& .MuiOutlinedInput-root': {
-                                        padding: '0 rem',
-                                        color:'black',
+                                    "& .MuiOutlinedInput-root": {
+                                        padding: "0 rem",
+                                        color: "black",
                                     },
                                 }}
                             />
@@ -112,41 +152,56 @@ const MessagePage = () => {
                         {filteredUsers.length === 0 ? (
                             <Typography>No users found.</Typography>
                         ) : (
-                            <Box component="ul" sx={{ listStyle: 'none', padding: 0 }}>
+                            <Box
+                                component='ul'
+                                sx={{
+                                    listStyle: "none",
+                                    padding: 0,
+                                    display: "grid",
+                                    gridTemplateColumns: "repeat(3, 1fr)", // 3 columns
+                                    gap: "1rem", // space between grid items
+                                    maxHeight: "350px", // adjust height as needed
+                                    overflowY: "auto", // enable scrolling if content exceeds max height
+                                }}
+                            >
                                 {filteredUsers.map((user) => (
-                                    <Box component="li" key={user._id} sx={{ marginBottom: '1.25rem' }}>
+                                    <Box component='li' key={user._id}>
                                         <Button
-                                            onClick={() => handleUserClick(user._id)}
+                                            onClick={() =>
+                                                handleUserClick(user._id)
+                                            }
                                             sx={{
-                                                textAlign: 'center',
-                                                padding: '1.0rem',
-                                                background: theme.palette.yellow.main,
-                                                border: '3px solid black',
-                                                borderRadius: '30px',
-                                                cursor: 'pointer',
-                                                display: 'flex',
-                                                alignItems: 'center',
-                                                color: 'black',
-                                                width: '30%',
-                                                fontFamily: theme.typography.retro,
-                                                textTransform: 'none',
-                                                '&:hover':{
-                                                    background: '#000099',
-                                                    borderColor: 'white',
-                                                    color: 'white',
-                                                }
+                                                textAlign: "center",
+                                                padding: "1.0rem",
+                                                background:
+                                                    theme.palette.yellow.main,
+                                                border: "3px solid black",
+                                                borderRadius: "30px",
+                                                cursor: "pointer",
+                                                display: "flex",
+                                                alignItems: "center",
+                                                color: "black",
+                                                width: "100%", // make the button fill the grid cell
+                                                fontFamily:
+                                                    theme.typography.retro,
+                                                textTransform: "none",
+                                                "&:hover": {
+                                                    background: "#000099",
+                                                    borderColor: "white",
+                                                    color: "white",
+                                                },
                                             }}
                                         >
                                             {user.profileImage && (
                                                 <Box
-                                                    component="img"
+                                                    component='img'
                                                     src={user.profileImage}
                                                     alt={user.name}
                                                     sx={{
-                                                        width: '30px',
-                                                        height: '30px',
-                                                        borderRadius: '50%',
-                                                        marginRight: '10px',
+                                                        width: "30px",
+                                                        height: "30px",
+                                                        borderRadius: "50%",
+                                                        marginRight: "10px",
                                                     }}
                                                 />
                                             )}
@@ -156,33 +211,38 @@ const MessagePage = () => {
                                 ))}
                             </Box>
                         )}
+
                         {openChats.map((userId) => (
                             <Dialog
                                 key={userId}
                                 open={true}
                                 onClose={() => handleCloseChat(userId)}
-                                aria-labelledby="chat-window-title"
-                                aria-describedby="chat-window-description"
+                                aria-labelledby='chat-window-title'
+                                aria-describedby='chat-window-description'
                             >
                                 <Box
                                     sx={{
                                         width: 400,
-                                        bgcolor: 'background.paper',
+                                        bgcolor: "background.paper",
                                         boxShadow: 24,
                                         p: 4,
-                                        border: '3px solid black',
-                                        borderRadius: '0px',
+                                        border: "3px solid black",
+                                        borderRadius: "0px",
                                         fontFamily: theme.typography.retro,
-                                        fontSize: 15,  
+                                        fontSize: 15,
                                         color: "#D3D3D3",
                                     }}
                                 >
                                     <ChatWindow userId={userId} />
-                                    <Button onClick={() => handleCloseChat(userId)}
+                                    <Button
+                                        onClick={() => handleCloseChat(userId)}
                                         sx={{
-                                            color:'black',
+                                            color: "black",
                                             fontFamily: theme.typography.retro,
-                                        }}>Close</Button>
+                                        }}
+                                    >
+                                        Close
+                                    </Button>
                                 </Box>
                             </Dialog>
                         ))}
